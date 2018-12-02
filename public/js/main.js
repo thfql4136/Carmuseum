a7213a42c37fc7dfaa8d4b3695a59c3da4815d63
unknown <thfql4136@naver.com>
2018-12-02 22:44:04 +0900

    181202

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,14 @@ $(window).scroll(function(){
 		if($("header").hasClass("dn_bg") == false) {
 			$("header").css({"top":"-15%"}).addClass("dn_bg");
 			$(".logo_tit").css({"color":"#c8c47b"});
+			$(".mo_tit").css({"color":"#c8c47b"});
 			$("header").stop().animate({"top":"0%"}, 500);
 		}
 	}
 	else {
 		$("header").css({"top":"-15%"}).removeClass("dn_bg");
 		$(".logo_tit").css({"color":"#fbf8c7"});
+		$(".mo_tit").css({"color":"#fbf8c7"});
 		$("header").stop().animate({"top":"0%"}, 500);
 	}
 });
