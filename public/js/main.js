e4f8c631157884e7faf94010d29e30e6e90c9bc5
thfql4136 <thfql4136@naver.com>
2018-12-10 14:25:06 +0900

    181210

--- public/js/main.js
+++ public/js/main.js
@@ -101,56 +101,6 @@ interval = setInterval(carAni, 3000);
 
 
 
- /*
- var cNum = 0;	//현재의 index
- var cLen = $(".car_img > li").length - 1;	//마지막 index (예:5개라면 0,1,2,3,4 -> 4)
- var duration = 500;	//animate 속도
- //최초 한번 실행
- $(window).resize(function(){
-	 //본 작업을 진행하는 이유는 absolute 되어 있는 객체의 높이를 계산하기 위해서..
-	 $(".car_img").height($(".car_img > li").eq(cNum).height()+30);
- }).trigger("resize");
-
- carPos();
- $(".car_img > li").each(function(i){
-
-	 carPos();
- });
- function carAni(val) {
-	 $(".car_img").height($(".car_img > li").eq(cNum).height() +30);
-	 $(".car_img > li").eq(cNum).css({"animation-name":"carAni", "animation-duration":duration*0.001+"s"});
-	 $(".car_img").stop().animate({"left":val}, duration, fbanPos);
- }
- function carPos() {
-	 $(".car_img > li").hide().css({"animation-name":""});
-	 $(".car_img > li").eq(cNum).show().css({"left":"44%","opacity":"1"});
-	 
-	 if(cNum == 0) {
-		$(".car_img > li").eq(cLen).show().css({"left":"10%"});
-		$(".car_img > li").eq(1).show().css({"left":"70%"});
-	 }
-	 else if(cNum == cLen) {
-		 $(".car_img > li").eq(cNum - 1).show().css({"left":"20%", "width":"45%"});
-		 $(".car_img > li").eq(0).show().css({"left":"70%", "width":"45%"});
-	 }
-	 else {
-		 $(".car_img > li").eq(cNum - 1).show().css({"left":"5%", "width":"45%"});
-		 $(".car_img > li").eq(cNum + 1).show().css({"left":"90%", "width":"45%"});
-		
-	 }
- }
- $(".che_left").click(function(){
-	 if(cNum == cLen) cNum = 0;
-	 else cNum++;
-	 carAni("100%");
- });
- $(".che_right").click(function(){
-	 if(cNum == 0) cNum = cLen;
-	 else cNum--;
-	 carAni("-50%");
- });
-*/
-
 
  var fNum = 0;	//현재의 index
 	var fLen = $(".fban > li").length - 1;	//마지막 index (예:5개라면 0,1,2,3,4 -> 4)
@@ -185,8 +135,8 @@ interval = setInterval(carAni, 3000);
 			$(".fac_img > li").eq(1).show().css({"left":"80%", "width":"45%", "top":"44%","opacity":"0.4"});
 		}
 		else if(fNum == fLen) {
-			$(".fac_img > li").eq(fNum - 1).show().css({"left":"-17%", "width":"45%", "top":"44%","opacity":"0.4"});
-			$(".fac_img > li").eq(0).show().css({"left":"80%", "width":"45%", "top":"44%","opacity":"0.4"});
+			$(".fac_img > li").eq(1).show().css({"left":"-17%", "width":"45%", "top":"44%","opacity":"0.4"});
+			$(".fac_img > li").eq(fLen).show().css({"left":"80%", "width":"45%", "top":"44%","opacity":"0.4"});
 		}
 		else {
 			$(".fac_img > li").eq(fNum - 1).show().css({"left":"-17%", "width":"45%", "top":"44%","opacity":"0.4"});
@@ -196,10 +146,18 @@ interval = setInterval(carAni, 3000);
 	$(".fac_left").click(function(){
 		if(fNum == fLen) fNum = 0;
 		else fNum++;
-		fbanAni("30%");
+		fbanAni("-30%");
 	});
 	$(".fac_right").click(function(){
 		if(fNum == 0) fNum = fLen;
 		else fNum--;
-		fbanAni("-30%");
+		fbanAni("30%");
 	});
+
+	var container = document.getElementById('map');
+	var options = {
+		center: new daum.maps.LatLng(33.284181, 126.349815),
+		level: 3
+	};
+
+	var map = new daum.maps.Map(container, options);
