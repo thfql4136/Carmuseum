b5ea7f74ccb8fe0edf378fb60183386d7939b730
booldook <booldook@gmail.com>
2018-12-10 16:02:09 +0900

    booldook

--- public/js/main.js
+++ public/js/main.js
@@ -41,7 +41,7 @@ $(".fa-bars").click(function(){
 });
 
 $('.movie_bg').tubular({ 
-    videoId: 'mAKsZ26SabQ',
+  videoId: 'mAKsZ26SabQ',
 	repeat:true
  });
 
@@ -99,9 +99,74 @@ function carAni() {
 
 interval = setInterval(carAni, 3000);
 
+var mNum = 0;
+var imgs = [];
+var decTit = [];
+var decCont = [];
+imgs[0] = "../img/facility1.png";
+imgs[1] = "../img/facility2.png";
+imgs[2] = "../img/facility3.png";
+imgs[3] = "../img/facility4.png";
+imgs[4] = "../img/facility5.png";
+decTit[0] = "전시관";
+decTit[1] = "박물관";
+decTit[2] = "전시관1";
+decTit[3] = "전시관2";
+decTit[4] = "전시관3";
+decCont[0] = "박물관에 들어서는 순간, 로비관에서는 전설의 명차로 기억되는 'Benz300SL','Patent', 'Bayard'을 만날 수 있습니다.";
+decCont[1] = "순간, 로비관에서는 전설의 명차로 기억되는 'Benz300SL','Patent', 'Bayard'을 만날 수 있습니다.";
+decCont[2] = "박물관에 들어서는 순간, 로비관에서는 'Benz300SL','Patent', 'Bayard'을 만날 수 있습니다.";
+decCont[3] = "박물관에 들어서는 순간, 로비관에서는 기억되는 'Benz300SL','Patent', 'Bayard'을 만날 수 있습니다.";
+decCont[4] = "박물관에 들어서는 순간,  'Benz300SL','Patent', 'Bayard'을 만날 수 있습니다.";
+var mEnd = imgs.length - 1;
+var mPic = $(".fac_img > li").eq(1);
+var pPic = $(".fac_img > li").eq(0);
+var lPic = $(".fac_img > li").eq(2);
+
+function mInit() {
+	$("img", ".fac_img").addClass( "fac_img_show" ).one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+		$(this).removeClass( "fac_img_show" );
+	});
+	if(mNum == 0) {
+		$("img", pPic).attr("src", imgs[mEnd]);
+		$("img", mPic).attr("src", imgs[mNum]);
+		$("img", lPic).attr("src", imgs[mNum+1]);
+	}
+	else if(mNum == mEnd) {
+		$("img", pPic).attr("src", imgs[mNum-1]);
+		$("img", mPic).attr("src", imgs[mNum]);
+		$("img", lPic).attr("src", imgs[0]);
+	}
+	else {
+		$("img", pPic).attr("src", imgs[mNum-1]);
+		$("img", mPic).attr("src", imgs[mNum]);
+		$("img", lPic).attr("src", imgs[mNum+1]);
+	}
+	$(".black_txt").children("h4").html(decTit[mNum]);
+	$(".black_txt").children("p").html(decCont[mNum]);
+}
+
+$(".fac_left").click(function(){
+	if(mNum == 0) mNum = mEnd;
+	else mNum--;
+	mInit();
+});
+
+$(".fac_right").click(function(){
+	if(mNum == mEnd) mNum = 0;
+	else mNum++;
+	mInit();
+});
+mInit();
 
 
+var rimInterval = setInterval(rimAni, 20);
+function rimAni(){
+	$(".rim").stop().css({"left":"-=1px"});
+}
 
+
+/*
  var fNum = 0;	//현재의 index
 	var fLen = $(".fban > li").length - 1;	//마지막 index (예:5개라면 0,1,2,3,4 -> 4)
 	var duration = 500;	//animate 속도
@@ -161,3 +226,5 @@ interval = setInterval(carAni, 3000);
 	};
 
 	var map = new daum.maps.Map(container, options);
+
+	*/
